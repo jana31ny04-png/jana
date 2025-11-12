4910328de7769e30a27830992c85f7ffb0bb0a0e
jana31ny04-png <jana31ny04@gmail.com>
2025-11-12 15:19:14 +0530

    Add files via upload

---
+++ food condition ja.py
@@ -0,0 +1,9 @@
+food = input("what is the best food?:")
+
+if food =="rice":
+    print("yes, rice the best.")
+
+elif food == "apple":
+     print("Apples aren't my cup of rice.")
+else:
+    print("Never heard of it!")
